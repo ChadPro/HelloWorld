e89ac7ed6d9489c9010298bc3c582ff55f48ced3
Stone Long <1193041759@qq.com>
2017-02-21 17:59:00 +0800

    网络-图片1
    网络-图片1

--- 网络/图片/ImageWebVC.m
+++ 网络/图片/ImageWebVC.m
@@ -7,8 +7,15 @@
 //
 
 #import "ImageWebVC.h"
+#import "SDAutoLayout.h"
 
-@interface ImageWebVC ()
+#define UIColorFromHex(s,alp)  [UIColor colorWithRed:(((s & 0xFF0000) >> 16))/255.0 green:(((s &0xFF00) >>8))/255.0 blue:((s &0xFF))/255.0 alpha:alp]
+
+@interface ImageWebVC ()<UITableViewDelegate,UITableViewDataSource>
+
+@property(nonatomic,strong) UITableView *tableView;
+@property(nonatomic,strong) NSArray *titles;
+@property(nonatomic,strong) NSString *cellIdentifier;
 
 @end
 
@@ -16,6 +23,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.titles = @[@"下载图片",@"上传图片",@"缓存图片"];
+    self.cellIdentifier = @"tableViewCell";
+    [self createUI];
+}
+
+- (void)createUI{
+    self.view.backgroundColor = UIColorFromHex(0x009966, 1.0);
+    
+    self.tableView = [[UITableView alloc]init];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:self.cellIdentifier];
+    [self.view addSubview:self.tableView];
+    
     
 }
 
