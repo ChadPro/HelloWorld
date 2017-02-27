1cab8cd31889762c04fd94db041753ffe9e4e425
Stone Long <1193041759@qq.com>
2017-02-27 17:55:25 +0800

    sqlite
    sqlite

--- 数据存储/SQLite3/HWSqliteVC.m
+++ 数据存储/SQLite3/HWSqliteVC.m
@@ -19,45 +19,97 @@
 
 static sqlite3 *_db;    //sqlite数据库 _db
 
+static int callback(void *notUsed, int argc,char **argv,char **colName){
+    
+    printf("argc = %d",argc);
+    printf("argc = %s",argv[1]);
+    
+    return 1;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = UIColorFromHex(0x009966, 1.0);
-    //********************
     
     NSString *documentsPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES).firstObject;
     NSLog(@"documentsPath = %@",documentsPath);
     
     NSString *filePath = [documentsPath stringByAppendingPathComponent:@"user.sqlite"];
     
-    //将OC字符串 -> C语言字符串
     const char *cfilePath = filePath.UTF8String;
-    //1.打开数据库
+    
+    //***************************分割线****************************************
+    //打开数据库
     int result = sqlite3_open(cfilePath, &_db);    //参数
     if(result == SQLITE_OK){
         NSLog(@"成功打开数据库");
-        
-    
-    //2.创表
-    const char *sql = "CREATE TABLE IF NOT EXISTS t_person (id integer PRIMARY KEY AUTOINCREMENT, name text NOT NULL, age integer NOT NULL);";
-    char *erroMsg = NULL;
-    result = sqlite3_exec(_db, sql, NULL, NULL, &erroMsg);
-    if (result == SQLITE_OK) {
-        NSLog(@"成功创表");
-    } else {
-        NSLog(@"创表失败--%s", erroMsg);
-      }
+
+        //[self searchData];
         
     }else{
-   
         NSLog(@"打开数据库失败");
     }
+    
+    //关闭数据库
+    sqlite3_close(_db);
+}
+
+//查询数据
+- (void)searchData{
+    const char *sql = "SELECT * FROM userTable";
+    char *erroMsg = NULL;
+    int result = sqlite3_exec(_db, sql, callback, NULL, &erroMsg);
+    if(result != SQLITE_OK){
+        printf("error = %s",erroMsg);
+    }
+}
+
+//插入数据
+- (void)insertData{
+    const char *sql = "INSERT INTO userTable (name,age) VALUES('Jack',26)";
+    char *erroMsg = NULL;
+    int result = sqlite3_exec(_db, sql, NULL, NULL, &erroMsg);
+    if(result == SQLITE_OK){
+        NSLog(@"插入成功");
+    }else{
+        printf("error = %s",erroMsg);
+    }
+    
+    const char *sql1 = "INSERT INTO userTable (name,age) VALUES('Tom',18)";
+    char *erroMsg1 = NULL;
+    int result1 = sqlite3_exec(_db, sql1, NULL, NULL, &erroMsg1);
+    if(result1 == SQLITE_OK){
+        NSLog(@"插入成功");
+    }else{
+        printf("error = %s",erroMsg);
+    }
+    
+    const char *sql2 = "INSERT INTO userTable (name,age) VALUES('Lucy',13)";
+    char *erroMsg2 = NULL;
+    int result2 = sqlite3_exec(_db, sql2, NULL, NULL, &erroMsg2);
+    if(result2 == SQLITE_OK){
+        NSLog(@"插入成功");
+    }else{
+        printf("error = %s",erroMsg);
+    }
+}
+
+//创建表
+- (void)createTable{
+      const char *sql = "CREATE TABLE IF NOT EXISTS userTable (id integer PRIMARY KEY AUTOINCREMENT, name text NOT NULL, age integer NOT NULL);";
+      char *erroMsg = NULL;
+      int result = sqlite3_exec(_db, sql, NULL, NULL, &erroMsg);
+    
+      if (result == SQLITE_OK) {
+                NSLog(@"成功创表");
+      } else {
+                NSLog(@"创表失败--%s", erroMsg);
+      }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 
 }
-
-
 
 @end
